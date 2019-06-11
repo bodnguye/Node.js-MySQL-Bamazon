66425f1d4be80b5fff94a593a370dde09da019f7
One Punch <bodnguye@gmail.com>
2019-06-10 21:40:02 -0700

    addInventory() completed

--- bamazonManager.js
+++ bamazonManager.js
@@ -3,7 +3,6 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const Table = require('cli-table');
 const clear = require('clear');
-clear();
 
 // create the connection information for the sql database
 const connection = mysql.createConnection({
@@ -29,6 +28,7 @@ connection.connect(function(err) {
 });
 
 function runManagerView() {
+clear();
   inquirer
     .prompt({
       name: "action",
@@ -77,10 +77,93 @@ function viewProduct() {
         })
 
         for (let i = 0; i < inventory.length; i++) {
-            table.push([ inventory[i].item_id, inventory[i].product_name, inventory[i].department_name, "$" + inventory[i].price, inventory[i].stock_quantity]);
+            table.push([inventory[i].item_id, inventory[i].product_name, inventory[i].department_name, "$" + inventory[i].price, inventory[i].stock_quantity]);
           }
-            console.log(table.toString());
-                })
+        console.log(`\n${table.toString()}\n\n`);
+        })
+        mainMenuPrompt();
+};
+
+function lowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function(err, inventory) {
+        if (err) throw err;
+
+        let table = new Table({
+            head: ['Item ID', 'Product', 'Department', 'Price', 'Stock Quantity'],
+            colWidths: [10, 68, 16, 10, 16],
+        })
+
+        for (let i = 0; i < inventory.length; i++) {
+            table.push([inventory[i].item_id, inventory[i].product_name, inventory[i].department_name, "$" + inventory[i].price, inventory[i].stock_quantity]);
+          }
+        console.log(`\n${table.toString()}\n\n`);
+        })
+        mainMenuPrompt();
+}
+
+function addInventory() {
+    connection.query("SELECT * FROM products", function(err, inventory) {
+        if (err) throw err;
+
+        let table = new Table({
+            head: ['Item ID', 'Product', 'Department', 'Price', 'Stock Quantity'],
+            colWidths: [10, 68, 16, 10, 16],
+        })
+
+        for (let i = 0; i < inventory.length; i++) {
+            table.push([inventory[i].item_id, inventory[i].product_name, inventory[i].department_name, "$" + inventory[i].price, inventory[i].stock_quantity]);
+          }
+        console.log(`\n${table.toString()}\n\n`);
+        })
+
+    inquirer
+        .prompt([
+              {
+                name: "inventory",
+                type: "input",
+                message: "Which item would you like to add more inventory? (Enter the item_id)",
+                validate: function(value) {
+                  if (isNaN(value) === false) {
+                    return true;
+                  }
+                    return false;
+                  }
+              },
+              {
+              name: "stock",
+              type: "input",
+              message: "What is the quantity would you like to add?",
+              validate: function(value) {
+                  if (isNaN(value) === false) {
+                    return true;
+                  }
+                    return false;
+                  }
+              }
+          ])
+          .then(function(answer) {
+            let query = "SELECT * FROM products WHERE ?";
+            connection.query(query, { item_id: answer.inventory }, function(err, selectedItem) {
+              if (err) throw err;
+              connection.query(
+                  "UPDATE products SET ? WHERE ?",
+                  [
+                    {
+                      stock_quantity: parseInt(selectedItem[0].stock_quantity) + parseInt(answer.stock)
+                    },
+                    {
+                      item_id: answer.inventory
+                    }
+                  ],
+
+                function(error) {
+                  if (error) throw err;
+                  console.log(`Item ID ${selectedItem[0].item_id} has been successfully updated`);
+                  mainMenuPrompt();
+                }
+                );
+            })
+          })
 };
 
 function mainMenuPrompt() {
@@ -92,7 +175,7 @@ function mainMenuPrompt() {
       choices: ["Main Menu", "Exit"]
       })
       .then(function(answer) {
-      // based on their answer, either call the start() or connection.end()
+      // based on their answer, either call the runManagerView() or connection.end()
         if (answer.mainMenu === "Main Menu") {
           runManagerView();
         }
